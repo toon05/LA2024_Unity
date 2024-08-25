2e1b3d70205d3f4e8a2513ddf748f45f70feb5ab
toon05 <109466656+toon05@users.noreply.github.com>
2024-08-25 21:23:29 +0900

    add

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class HelloWorld : MonoBehaviour
+{
+    string[] message;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 配列を正しい形式で初期化
+        message = new string[] { "Hello", "World", "Life", "is", "Tech!" };
+
+        // 非同期メソッドの呼び出し
+        LogMessage().Forget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    async UniTask LogMessage()
+    {
+        foreach (var m in message)
+        {
+            Debug.Log(m);
+            await UniTask.Delay(1000); // 1秒間待機
+        }
+    }
+}
