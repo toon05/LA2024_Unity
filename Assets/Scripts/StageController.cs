e22addb76e5b970f333c51748aa576d39c45d2e1
toon05 <109466656+toon05@users.noreply.github.com>
2024-09-01 23:50:50 +0900

    [add]

---
+++ Assets/Scripts/StageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+	public GameObject player;
+	Vector3 pivotDirection;
+	public float rotateSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		pivotDirection = new Vector3(0, 0, 1);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			transform.RotateAround(player.transform.position, pivotDirection, rotateSpeed);
+		}
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			transform.RotateAround(player.transform.position, pivotDirection, -rotateSpeed);
+		}
+	}
+}
