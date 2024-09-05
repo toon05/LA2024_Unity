a2d016d8b931e5b413fbe6cc897df1e40979d3fa
toon05 <109466656+toon05@users.noreply.github.com>
2024-09-05 23:03:02 +0900

    [add] transform re

---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+    }
+}
